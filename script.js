adc74cda7d35a2caa64eaa5303eeee5de5751dff
Heena <heena@Heenas-MacBook-Pro.local>
2024-01-08 21:16:53 +0530

    Add. More program using React Fragments

--- script.js
+++ script.js
@@ -3,13 +3,25 @@
 // header.className = "header";
 // // document.getElementsByClassName("root")[0].append(header);
 // document.getElementById("root").append(header);
-// console.log("JAVASCRIPT Element",header); 
-
+// console.log("JAVASCRIPT Element",header);
 
 // const reactHeader=React.createElement("h1",{className:"head", id:"reactHead", children:"Hello React!!"});
 // ReactDOM.createRoot(document.getElementById("root")).render(reactHeader);
 // console.log("REACT Element",reactHeader);
 
+const BabelHeader = (
+  <>
+    <h1>Hello JSX</h1>
+    <p>This is Paragraph</p>
+    <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JAVASCRIPT</li>
+        <li>REACT</li>
+   
 
-const BabelHeader=(<h1>Hello JSX</h1>);
+    </ul>
+  </>
+);
+
 ReactDOM.createRoot(document.getElementById("root")).render(BabelHeader);
