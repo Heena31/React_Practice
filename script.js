4111dfd017e173c7b7d793fdc97d0c02bacc1ba5
Heena <heena@Heenas-MacBook-Pro.local>
2024-01-08 21:18:41 +0530

    Create. Function Component

--- script.js
+++ script.js
@@ -9,19 +9,27 @@
 // ReactDOM.createRoot(document.getElementById("root")).render(reactHeader);
 // console.log("REACT Element",reactHeader);
 
-const BabelHeader = (
-  <>
-    <h1>Hello JSX</h1>
-    <p>This is Paragraph</p>
-    <ul>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>JAVASCRIPT</li>
-        <li>REACT</li>
+// const BabelHeader = (
+//   <>
+//     <h1>Hello JSX</h1>
+//     <p>This is Paragraph</p>
+//     <ul>
+//         <li>HTML</li>
+//         <li>CSS</li>
+//         <li>JAVASCRIPT</li>
+//         <li>REACT</li>
    
 
-    </ul>
-  </>
-);
+//     </ul>
+//   </>
+// );
 
-ReactDOM.createRoot(document.getElementById("root")).render(BabelHeader);
+function App()
+{
+    return <>
+      <h1>Hello JSX</h1>
+      <p>This is Paragraph</p>
+    </>
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App/>);
