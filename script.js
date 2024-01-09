24288ca039a4138010f758f9996f45d363ef943e
Heena <heena@Heenas-MacBook-Pro.local>
2024-01-09 23:03:02 +0530

    Embeding Js in Jsx

--- script.js
+++ script.js
@@ -1,35 +1,22 @@
-// const header = document.createElement("h2");
-// header.textContent = "Hello JavaScript Element";
-// header.className = "header";
-// // document.getElementsByClassName("root")[0].append(header);
-// document.getElementById("root").append(header);
-// console.log("JAVASCRIPT Element",header);
-
-// const reactHeader=React.createElement("h1",{className:"head", id:"reactHead", children:"Hello React!!"});
-// ReactDOM.createRoot(document.getElementById("root")).render(reactHeader);
-// console.log("REACT Element",reactHeader);
-
-// const BabelHeader = (
-//   <>
-//     <h1>Hello JSX</h1>
-//     <p>This is Paragraph</p>
-//     <ul>
-//         <li>HTML</li>
-//         <li>CSS</li>
-//         <li>JAVASCRIPT</li>
-//         <li>REACT</li>
-   
-
-//     </ul>
-//   </>
-// );
-
-function App()
+var name="World";
+var age=21;
+let demo=null;
+let undef;
+let bool=true;
+function sum(a,b)
 {
-    return <>
-      <h1>Hello JSX</h1>
-      <p>This is Paragraph</p>
-    </>
+    return a+b;
 }
+const App=()=>(
+    <>
+    <h1>Hello{name}!</h1>
+    <p>Your age is {age}.</p>
+    <p>Value of null is :{demo}</p>
+    <p>Value of undefined is : {undef}</p>
+    <p>Value of Boolean variable is: {bool}</p>
+    <p>Sum: {sum(5,6)}</p>
+    </>
+)
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App/>);
+
